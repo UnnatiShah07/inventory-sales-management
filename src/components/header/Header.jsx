119ffd4159bf2df572c42de6ea7f2c70de5f3824
UnnatiShah07 <unnatishah7101@gmail.com>
2024-05-19 15:51:56 +0530

    added links for code base

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import "./header.css";
+import { SiReplit, SiGithub } from "react-icons/si";
 
 export const Header = () => {
   return (
@@ -14,6 +15,12 @@ export const Header = () => {
         <Link className="link" to="/sales">
           Sales
         </Link>
+        <a href="https://github.com/UnnatiShah07/inventory-sales-management" target="_blank" rel="noreferrer" className="code-link">
+          <SiGithub />
+        </a>
+        <a href="https://replit.com/@UnnatiShah07/Assignment-18-Inventory-Sales" target="_blank" rel="noreferrer" className="code-link">
+          <SiReplit />
+        </a>
       </div>
     </div>
   );
